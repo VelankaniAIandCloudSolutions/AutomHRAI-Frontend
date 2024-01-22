7416b1f392c714f7ea101ac92cded7c81a1f58b2
VelankaniAIandCloudSolutions <aicloudsolutions@velankanigroup.com>
2024-01-22 18:11:25 +0530

    added parser models

--- src/components/Layout/AppSidebar.js
+++ src/components/Layout/AppSidebar.js
@@ -5,7 +5,7 @@ export default function AppSidebar() {
     <aside className="main-sidebar sidebar-dark-primary elevation-4">
       <a href="/" className="brand-link">
         <img
-          src="https://content.jdmagicbox.com/comp/bangalore/73/080ppe03673/catalogue/velankani-software-pvt-ltd-electronic-city-bangalore-computer-software-developers-2rgz2h5-250.jpg"
+          src="/automhrlogo.png"
           alt="AdminLTE Logo"
           className="brand-image img-circle elevation-3"
           style={{ opacity: 0.8 }}
@@ -21,17 +21,52 @@ export default function AppSidebar() {
           >
             <li className="nav-item text-left">
               <a href="/" className="nav-link">
-                <i className="nav-icon fas fa-users"></i>
+                <i className="nav-icon fas fa-file"></i>
                 <p>
-                  Accounts
+                  Resume Parser
                   <i className="right fas fa-angle-left"></i>
                 </p>
               </a>
               <ul className="nav nav-treeview">
-                <li class="nav-item">
+                <li className="nav-item">
                   <a href="!" className="nav-link">
                     <i className="far fa-circle nav-icon"></i>
-                    <p>Test</p>
+                    <p>Dashboard</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="!" className="nav-link">
+                    <i className="far fa-circle nav-icon"></i>
+                    <p>Resumes</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul
+            className="nav nav-pills nav-sidebar flex-column"
+            data-widget="treeview"
+            role="menu"
+          >
+            <li className="nav-item text-left">
+              <a href="/" className="nav-link">
+                <i className="nav-icon fas fa-users"></i>
+                <p>
+                  Candidate Matching
+                  <i className="right fas fa-angle-left"></i>
+                </p>
+              </a>
+              <ul className="nav nav-treeview">
+                <li className="nav-item">
+                  <a href="!" className="nav-link">
+                    <i className="far fa-circle nav-icon"></i>
+                    <p>Candidates</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="!" className="nav-link">
+                    <i className="far fa-circle nav-icon"></i>
+                    <p>Job Descriptions</p>
                   </a>
                 </li>
               </ul>
