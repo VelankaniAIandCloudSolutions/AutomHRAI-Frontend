967160595f689306ca0dfdd65299d18924006e90
Ankit Raj <ankitrajgaya1996@gmail.com>
2024-02-26 09:17:48 +0530

    Candidate_Ranking

--- src/components/CandidateRanking/Leaderboard.js
+++ src/components/CandidateRanking/Leaderboard.js
@@ -1,8 +1,8 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 const Leaderboard = ({ data }) => {
-
-  const itemsPerPage = 10; // Adjust this value to set the number of items per page
+  console.log('DATA:',data);
+  const itemsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(1);
 
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -18,45 +18,30 @@ const Leaderboard = ({ data }) => {
   return (
     <div id="leaderboard" className="position-relative">
       <table className="table table-hover table-bordered mt-3">
-        {data.map((item, index) => (
+        {currentItems.map((item, index) => (
           <tr key={index}>
-            <td className="number">{index + 1} </td>
+            <td className="number">{index + 1}</td>
             <td>
-  <div style={{ display: 'flex', alignItems: 'center', background:'transparent' }}>
-    <div className='photo' style={{ width: '40px', height: '40px', overflow: 'hidden', borderRadius: '50%', display: 'flex', justifyContent: 'center' }}>
-      <img src={item.imageSrc} alt="gold medal" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-    </div>
-    <span className="name" style={{ marginLeft: '8px' }}>{item.name}</span>
-  </div>
-</td>
-
-
-
-            {/* <td className="name">{item.name} </td> */}
+              <div style={{ display: 'flex', alignItems: 'center', background: 'transparent' }}>
+                <div className='photo' style={{ width: '40px', height: '40px', overflow: 'hidden', borderRadius: '50%', display: 'flex', justifyContent: 'center' }}>
+                  <img src={item.imageSrc} alt="user" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                </div>
+                <span className="name" style={{ marginLeft: '8px' }}>{item.name}</span>
+              </div>
+            </td>
             <td className="points">
-            <button type="button" className="btn btn-success btn-sm mt-3 p-1" style={{height:'30px',width:'30px', border: 'none',backgroundColor: '#007bff',color:'#eeee' }}>
-                  <i className="fas fa-download" style={{ alignItems: 'center' }}></i>
-                </button>
-
+              <button type="button" className="btn btn-success btn-sm mt-3 p-1">
+                <i className="fas fa-download"></i>
+              </button>
               <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', background: 'transparent' }}>
-               
-                <span className="points-value">{item.points}</span>
+                <span className="points-value">{item.scores}</span>
                 <div className="progress" style={{ height: '20px', width: '50px' }}>
-                  <div className="progress-bar" role="progressbar" style={{ width: `${item.points}%` }} aria-valuenow={item.points} aria-valuemin={0} aria-valuemax={100}></div>
+                  <div className="progress-bar" role="progressbar" style={{ width: `${item.scores}%` }} aria-valuenow={item.scores} aria-valuemin={0} aria-valuemax={100}></div>
                 </div>
               </div>
             </td>
-
-
-  
-
-  {/* <td>
-    <button type="button" className="btn btn-primary btn-sm">Download</button>
-  </td> */}
-
-            
           </tr>
-        ))} 
+        ))}
       </table>
       <nav aria-label="Page navigation example">
         <ul className="pagination justify-content-end">
@@ -81,7 +66,6 @@ const Leaderboard = ({ data }) => {
           </li>
         </ul>
       </nav>
-
     </div>
   );
 };
