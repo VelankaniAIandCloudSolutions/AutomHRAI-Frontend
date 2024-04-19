8f22f3edf91482ec4ab2daf77447776233b625b5
satvik <katochsatvik@gmail.com>
2024-04-19 17:49:19 +0530

    fixed destroy bug

--- src/pages/Accounts/EditUser.js
+++ src/pages/Accounts/EditUser.js
@@ -71,31 +71,29 @@ function EditUser() {
       });
   };
 
-  useEffect(async () => {
-    dispatch(showLoading());
-    // Fetch user details based on the id when the component mounts
-    try {
-      const response = await axios.get(`/accounts/users/${id}`);
-      // Update state with fetched user data
-      setUserData(response.data.user);
-      console.log(response.data);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      dispatch(showLoading());
+      try {
+        const response = await axios.get(`/accounts/users/${id}`);
+        console.log(response.data); // Logging the whole user data response
 
-      setUserData((prevData) => ({
-        ...prevData,
-        emp_id: response.data.user.emp_id,
-      }));
-      // setInitialImage(`${response.data.user.user_image}`);
-      setCurrentImage(`${response.data.user.user_image}`); // Set initial image URL
-      console.log(`${response.data.user.user_image.split("/").pop()}`);
-      const LabelImg = response.data.user.user_image.split("/").pop();
-      setImageLabel(LabelImg);
-      dispatch(hideLoading());
-    } catch (error) {
-      console.error("Error fetching user details:", error);
-      dispatch(hideLoading());
-    }
-  }, [dispatch, id]); // Include id in the dependency array to re-run the effect when id changes
+        setUserData(response.data.user);
+        // Assuming response.data.user contains `emp_id` and `user_image` properties
+        setCurrentImage(response.data.user.user_image); // Set initial image URL
 
+        const imageName = response.data.user.user_image.split("/").pop(); // Extracting image name from URL
+        console.log(imageName); // Logging the image name
+        setImageLabel(imageName); // Setting the image label state
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      } finally {
+        dispatch(hideLoading());
+      }
+    };
+
+    fetchUserData();
+  }, [id, dispatch]);
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     setUserData((prevData) => ({
