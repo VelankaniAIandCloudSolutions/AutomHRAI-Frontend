8061f1130fdd2f6e2ab0cfc73887a588c7c2f336
satvik <katochsatvik@gmail.com>
2024-05-04 12:27:01 +0530

    report as csv done

--- src/components/FaceRecognition/AgGridMonthlyContractWorkerAttendanceReport.js
+++ src/components/FaceRecognition/AgGridMonthlyContractWorkerAttendanceReport.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
@@ -12,6 +12,7 @@ function AgGridMonthlyContractWorkerAttendanceReport({
   const [selectedProjectId, setSelectedProjectId] = useState(null);
   const [modalData, setModalData] = useState(null);
   const [selectedRows, setSelectedRows] = useState([]);
+  const gridApiRef = useRef(null);
 
   const handleDeleteClick = (projectId) => {
     setSelectedProjectId(projectId);
@@ -28,29 +29,6 @@ function AgGridMonthlyContractWorkerAttendanceReport({
     console.log("The rows", selectedRows);
   };
 
-  function ActionsCellRenderer(props) {
-    const handleViewClick = () => {
-      const rowData = props.data; // Get the row data
-      // Pass the rowData to your modal component
-      // For simplicity, let's assume you have a function to set modal data
-      console.log(" this is the rowDAta being passed in the modal", rowData);
-      setModalData(rowData.entries);
-    };
-
-    return (
-      <div className="p-0">
-        <button
-          className="btn btn-primary btn-sm"
-          onClick={handleViewClick}
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          <i className="fas fa-eye"></i>
-        </button>
-      </div>
-    );
-  }
-
   const formatTimeFromSeconds = (seconds) => {
     if (seconds === null || isNaN(seconds)) {
       return "-";
@@ -61,106 +39,56 @@ function AgGridMonthlyContractWorkerAttendanceReport({
     }
   };
 
-  //   const generateDynamicColumns = (data) => {
-  //     const dynamicColumns = [];
-  //     const dates = Object.keys(data[0]).filter(
-  //       (key) =>
-  //         key !== "contract_worker_name" &&
-  //         key !== "agency" &&
-  //         key !== "subcategory"
-  //     );
-  //     for (const date of dates) {
-  //       dynamicColumns.push({
-  //         headerName: date,
-  //         children: [
-  //           {
-  //             headerName: "Status",
-  //             field: `${date}.status`,
-  //             width: 120,
-  //             filter: true,
-  //           },
-  //           {
-  //             headerName: "Effective Working Time",
-  //             field: `${date}.effective_working_time`,
-  //             width: 200,
-  //             valueFormatter: ({ value }) => formatTimeFromSeconds(value),
-  //           },
-  //           {
-  //             headerName: "Total Break Time",
-  //             field: `${date}.total_break_time`,
-  //             width: 180,
-  //             valueFormatter: ({ value }) => formatTimeFromSeconds(value),
-  //           },
-  //         ],
-  //       });
-  //     }
-  //     return dynamicColumns;
-  //   };
+  const handleExportCSV = () => {
+    if (gridApiRef.current && rowData && rowData.length > 0) {
+      const columnDefs = gridApiRef.current.getColumnDefs();
+      const headers = columnDefs.map((colDef) => colDef.headerName);
 
-  //   const generateDynamicColumns = (data) => {
-  //     const dynamicColumns = [];
-  //     if (data && data.length > 0) {
-  //       const dates = Object.keys(data[0]).filter(
-  //         (key) =>
-  //           key !== "contract_worker_name" &&
-  //           key !== "agency" &&
-  //           key !== "subcategory"
-  //       );
-  //       for (const date of dates) {
-  //         dynamicColumns.push({
-  //           headerName: date,
-  //           field: date,
-  //           width: 300,
-  //           valueFormatter: ({ data }) => {
-  //             const rowData = data[date];
-  //             return `${rowData ? rowData.status : ""} - ${formatTimeFromSeconds(
-  //               rowData ? rowData.effective_working_time : 0
-  //             )}`;
-  //           },
-  //         });
-  //       }
-  //     }
-  //     return dynamicColumns;
-  //   };
+      const csvData = rowData.map((row) => {
+        const rowDataForExport = columnDefs.map((colDef) => {
+          const field = colDef.field;
+          if (field in row) {
+            const value = row[field];
+            if (typeof value === "object" && value !== null) {
+              return `${value.status} - ${formatTimeFromSeconds(
+                value.effective_working_time
+              )}`;
+            } else {
+              return value;
+            }
+          } else {
+            return "";
+          }
+        });
+        return rowDataForExport;
+      });
 
-  //   const generateDynamicColumns = (data) => {
-  //     const dynamicColumns = [];
-  //     if (data && data.length > 0) {
-  //       const dates = Object.keys(data[0]).filter(
-  //         (key) =>
-  //           key !== "contract_worker_name" &&
-  //           key !== "agency" &&
-  //           key !== "subcategory"
-  //       );
-  //       for (const date of dates) {
-  //         dynamicColumns.push({
-  //           headerName: date,
-  //           field: date,
-  //           width: 150,
-  //           cellRenderer: (params) => {
-  //             const rowData = params.data[date];
-  //             const status = rowData ? rowData.status : "";
-  //             let statusColor = "";
-  //             if (status === "Absent") {
-  //               statusColor = "red"; // Change to red for Absent status
-  //             } else if (status === "Present") {
-  //               statusColor = "darkgreen"; // Change to dark green for Present status
-  //             }
-  //             return (
-  //               <div>
-  //                 <span style={{ color: statusColor }}>{status.charAt(0)}</span> -{" "}
-  //                 {formatTimeFromSeconds(
-  //                   rowData ? rowData.effective_working_time : 0
-  //                 )}
-  //               </div>
-  //             );
-  //           },
-  //           autoHeight: true, // To ensure the cell height adjusts based on content
-  //         });
-  //       }
-  //     }
-  //     return dynamicColumns;
-  //   };
+      const csvRows = [headers.join(",")];
+      csvData.forEach((row) => {
+        csvRows.push(row.join(","));
+      });
+
+      const csvContent = csvRows.join("\n");
+
+      const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+      const link = document.createElement("a");
+      if (link.download !== undefined) {
+        const url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute(
+          "download",
+          "monthly_contract_worker_attendance_report.csv"
+        );
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }
+  };
+
+  const onGridReady = (params) => {
+    gridApiRef.current = params.api; // Store grid API reference
+  };
 
   const generateDynamicColumns = (data) => {
     const dynamicColumns = [];
@@ -233,84 +161,22 @@ function AgGridMonthlyContractWorkerAttendanceReport({
 
   return (
     <div>
-      <div className="ag-theme-quartz" style={{ height: 500 }}>
-        <AgGridReact rowData={rowData} columnDefs={colDefs} />
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <button className="btn-sm btn-secondary mt-3" onClick={handleExportCSV}>
+          Export
+          <i className="fas fa-download ms-2" />
+        </button>
       </div>
-
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-xl">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">
-                All Entries
-              </h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">
-              <ContractWorkerAttendanceGrid
-                attendanceData={modalData}
-                onSelectionChange={handleSelectionChange}
-                showImgInNewWindow={true}
-              />
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              {/* <button
-                type="button"
-                className="btn btn-primary"
-                onClick={handleConfirmDelete}
-                data-bs-dismiss="modal"
-              >
-                Confirm
-              </button> */}
-            </div>
-          </div>
-        </div>
+      <div className="ag-theme-quartz mt-2" style={{ height: 500 }}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={colDefs}
+          onGridReady={onGridReady}
+          // Enable CSV Export
+        />
       </div>
     </div>
   );
 }
 
 export default AgGridMonthlyContractWorkerAttendanceReport;
-
-// dynamicColumns.push({
-//     headerName: formattedDate,
-//     field: date,
-//     width: 100,
-//     cellRenderer: (params) => {
-//       const rowData = params.data[date];
-//       const status = rowData ? rowData.status : "";
-//       let statusColor = "";
-//       if (status === "Absent") {
-//         statusColor = "red"; // Change to red for Absent status
-//       } else if (status === "Present") {
-//         statusColor = "darkgreen"; // Change to dark green for Present status
-//       }
-//       return (
-//         <div>
-//           <span style={{ color: statusColor }}>{status.charAt(0)}</span> -{" "}
-//           {formatTimeFromSeconds(
-//             rowData ? rowData.effective_working_time : 0
-//           )}
-//         </div>
-//       );
-//     },
-//     autoHeight: true, // To ensure the cell height adjusts based on content
-//   });
